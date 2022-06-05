ad2056cd47121471215497b2d28540fed6c2f977
Priya Patel <83440686+Priya-Patel31@users.noreply.github.com>
2022-06-06 00:39:08 +0530

    Profile page UI (#2)
    * feat : added homepage UI
    
    * feat : added profile page

--- src/shared/leftSidebar/LeftSidebar.tsx
+++ src/shared/leftSidebar/LeftSidebar.tsx
@@ -6,26 +6,35 @@ import {
   BsFillBookmarkFill,
   MdExplore,
 } from "../../assets/icons/icons";
+import { NavLink } from "react-router-dom";
 
 const LeftSidebar = () => {
   return (
     <aside className="leftSidebar-container">
       <ul className="leftSidebar-list-items list-style-none">
-        <li className="leftSidebar-list-item active font-bold">
-          <AiFillHome className="leftSidebar-list-icon" />
-          <a href="/" className="list-item-label">Home</a>
+        <li className="leftSidebar-list-item font-bold">
+          <NavLink to="/" className="flex-row align-center">
+            <AiFillHome className="leftSidebar-list-icon" />
+            <span className="list-item-label">Home</span>
+          </NavLink>
         </li>
         <li className="leftSidebar-list-item font-bold">
-          <MdExplore className="leftSidebar-list-icon" />
-          <a href="/" className="list-item-label">Explore</a>
+          <NavLink to="/explore" className="flex-row align-center">
+            <MdExplore className="leftSidebar-list-icon" />
+            <span className="list-item-label">Explore</span>
+          </NavLink>
         </li>
         <li className="leftSidebar-list-item font-bold">
-          <BsFillBookmarkFill className="leftSidebar-list-icon" />
-          <a href="/" className="list-item-label">Bookmarks</a>
+          <NavLink to="/bookmark" className="flex-row align-center">
+            <BsFillBookmarkFill className="leftSidebar-list-icon" />
+            <span className="list-item-label">Bookmarks</span>
+          </NavLink>
         </li>
         <li className="leftSidebar-list-item font-bold">
-          <FaUserCircle className="leftSidebar-list-icon" />
-          <a href="/" className="list-item-label">Profile</a>
+          <NavLink to="/profile" className="flex-row align-center">
+            <FaUserCircle className="leftSidebar-list-icon" />
+            <span className="list-item-label">Profile</span>
+          </NavLink>
         </li>
       </ul>
       <button className="button primary-button create-post-btn font-bold">
